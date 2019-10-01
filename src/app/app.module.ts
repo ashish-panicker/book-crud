0d14bd1583e206164a92f9569afa2d6779a9eb6f
ashish-panicker <ashish.s.panicker@gmail.com>
2019-10-01 12:12:17 +0530

    first setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AddComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
