b8c30d810525bda97c6b64d0d8716c4d8510bce8
ashish-panicker <ashish.s.panicker@gmail.com>
2019-10-01 13:41:46 +0530

    third

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {Book} from './book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  books:Book[] = [];
+
   constructor() { }
+
+
 }
